bb5b025e4428a7bf611aed3d7d1b56454b02cecd
Henrique <henriqueamorim194@gmail.com>
2026-07-26 11:02:02 -0300

    criando o restante das rotas pro banco de dados

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,8 +2,26 @@ import livro from "../models/livro.js";
 
 class LivroController {
   static async listarLivros(req, res) {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.menssage} - falha na requisição` });
+    }
+  }
+
+  static async listarLivroPorID(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.menssage} - falha na requisição do livro` });
+    }
   }
 
   static async cadastrarLivro(req, res) {
@@ -14,6 +32,30 @@ class LivroController {
       res
         .status(500)
         .json({ message: `${erro.message} - falha ao cadastrar livro` });
+    }
+  }
+
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "Livro atualizado" });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.menssage} - falha na atualização do livro` });
+    }
+  }
+
+  static async deletarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(200).json({ message: "Livro Deletado" });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.menssage} - falha ao deletar o livro` });
     }
   }
 }
