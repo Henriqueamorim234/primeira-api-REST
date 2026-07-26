99aa4f7085268ff87e7026fc64a50be1504a4873
Henrique <henriqueamorim194@gmail.com>
2026-07-26 11:26:48 -0300

    adicionando rotas para autores

---
+++ src/controllers/autorController.js
@@ -0,0 +1,63 @@
+import { autor } from "../models/Autor.js";
+
+class AutorController {
+  static async listarAutor(req, res) {
+    try {
+      const autorLivros = await autor.find({});
+      res.status(200).json(listaLivros);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.menssage} - falha na requisição` });
+    }
+  }
+
+  static async listarAutorPorID(req, res) {
+    try {
+      const id = req.params.id;
+      const autorEncontrado = await autor.findById(id);
+      res.status(200).json(autorEncontrado);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.menssage} - falha na requisição do autor` });
+    }
+  }
+
+  static async cadastrarAutor(req, res) {
+    try {
+      const novoAutor = await autor.create(req.body);
+      res.status(201).json({ message: "criado com sucesso", livro: novoAutor });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha ao cadastrar autor` });
+    }
+  }
+
+  static async atualizarAutor(req, res) {
+    try {
+      const id = req.params.id;
+      await autor.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "Autor atualizado" });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.menssage} - falha na atualização do autor` });
+    }
+  }
+
+  static async deletarAutor(req, res) {
+    try {
+      const id = req.params.id;
+      await autor.findByIdAndDelete(id);
+      res.status(200).json({ message: "Autor Deletado" });
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.menssage} - falha ao deletar o autor` });
+    }
+  }
+}
+
+export default AutorController;
